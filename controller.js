7da14baf0f91b0d0339930e516684e18ac817ead
mailinnmoneel <84009244+mailinnmoneel@users.noreply.github.com>
2021-06-22 14:14:21 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,38 +1,34 @@
-
-//Funksjon for å velge stolpe. 
 function choosePillar(id) 
 {   
     if (selectedPillar == id) selectedPillar = null;
     else selectedPillar = id;
 
-    CheckIfChosen();
+    checkIfChosen();
     
 
     show();
 }
 
-//Funksjon for å velge valgt stolpe
-function CurrentPillar()
+function currentPillar()
 {
     return selectedPillar == null ? "Ingen" : selectedPillar;
 }
-//Funksjon for å disable/enable knappene
-function CheckIfChosen()
+
+function checkIfChosen()
 {
      selectedPillar == null ? status = "disabled" : status = "enabled";
 }
 
-//funksjon for å slette en stolpe
+
 function deletePillar() 
-    {
+{
     numbers.splice (selectedPillar -1, 1);
 
     show()
 }
 
-//Funksjon for å endre en stolpe til verdi i inputfeltet
 function changePillar() 
-    {      
+{      
     if (inputValue > 0 && inputValue <= 10) 
     {
         numbers[selectedPillar -1] = inputValue;   
@@ -44,13 +40,13 @@ function changePillar()
    show()
 }
 
-
-//Funksjon for å legge til en stolpe
 function addPillar() 
-    {
+{
+    
+
     if (inputValue > 0 && inputValue <= 10) 
-    {
-         numbers.push(inputValue); 
+    {        
+        numbers.push(inputValue); 
     }           
     else
     {
